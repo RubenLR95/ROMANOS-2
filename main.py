a81746b36346610e7a14472ab8ee9f80bdf35ff5
RubenLR95 <ruben.lopez.ras@gmail.com>
2024-03-09 11:31:07 +0100

    primera version entero a romano

--- main.py
+++ main.py
@@ -26,36 +26,35 @@ decenas={10: 'X' , 20: 'XX' , 30: 'XXX' ,  40: 'XV' , 50: 'L' , 60: 'LX' , 70: '
 centenas={100: 'C' , 200: 'CC' , 300: 'CCC' ,400: 'CD' , 500: 'D' , 600: 'DC' ,700: 'DCC' , 800: 'DCCC' , 900: 'CM'}
 millares={1000: 'M' , 2000: 'MM' , 3000: 'MMM'}
 
+dic_entero_a_romano= {1:'I' ,2: 'II' ,3: 'III' ,4: 'IV' , 5: 'V' , 6: 'VI' ,  7: 'VII' , 8: 'VIII', 9: 'IX', 10: 'X' , 20: 'XX' , 30: 'XXX' ,  40: 'XV' , 50: 'L' , 60: 'LX' , 70: 'LXX' , 80: 'LXXX' , 90: 'XC', 100: 'C' , 200: 'CC' , 300: 'CCC' ,400: 'CD' , 500: 'D' , 600: 'DC' ,700: 'DCC' , 800: 'DCCC' , 900: 'CM', 1000: 'M' , 2000: 'MM' , 3000: 'MMM'}
 
 class RomanNumberError( Exception ):
     pass
-
-#1994
-
-
+1994
 def entero_a_romano(numero):#1994
     numero = str(numero)
     list_numero = list(numero)
-
+   
     valor_romano = ""
-    
 
     for i in range(0,len(list_numero)):
-        if i==0:
+        if  i==0:
             list_numero[i] = int( list_numero[i] )*1000
-            valor_romano += millares.get(list_numero[i])
+            valor_romano += millares.get( list_numero[i])
+
         elif i==1:
             list_numero[i] = int( list_numero[i] )*100
             valor_romano += centenas.get(list_numero[i])
+
         elif i==2:
             list_numero[i] = int( list_numero[i] )*10
             valor_romano += decenas.get(list_numero[i])
+
         elif i==3: 
             list_numero[i] = int( list_numero[i] )
             valor_romano += unidades.get(list_numero[i])
-    
 
-        
+
     return valor_romano
 
-print( entero_a_romano(1994))
+print( entero_a_romano(1994) )
--- test_pruebas_unitarias_romanos.py
+++ test_pruebas_unitarias_romanos.py
@@ -1,5 +1,7 @@
-def test_prueba():
-    assert 10<20
+from main import entero_a_romano
 
-def test_prueba():
-    assert 10!=20
+valor_romanos = entero_a_romano(1994)
+
+
+def test_prueba_entero_a_romano():
+    assert  valor_romano == "MCMXCIV"
