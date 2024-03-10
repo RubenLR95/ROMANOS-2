b868b4aa6d6e2b433abb9e61e22d5a8eab1c3e23
RubenLR95 <ruben.lopez.ras@gmail.com>
2024-03-10 14:21:09 +0100

    agregado de funcion entero a romano terminado con pruebas unitarias

--- main.py
+++ main.py
@@ -20,48 +20,31 @@
 #I → 1
 
 
-diccionario = {1000: "M", 500: "D", 100: "C" , 50: "L" , 10: "X" , 5: "V" , 1: "I"}
-unidades={1:'I' ,2: 'II' ,3: 'III' ,4: 'IV' , 5: 'V' , 6: 'VI' ,  7: 'VII' , 8: 'VIII', 9: 'IX'}
-decenas={10: 'X' , 20: 'XX' , 30: 'XXX' ,  40: 'XV' , 50: 'L' , 60: 'LX' , 70: 'LXX' , 80: 'LXXX' , 90: 'XC'}
-centenas={100: 'C' , 200: 'CC' , 300: 'CCC' ,400: 'CD' , 500: 'D' , 600: 'DC' ,700: 'DCC' , 800: 'DCCC' , 900: 'CM'}
-millares={1000: 'M' , 2000: 'MM' , 3000: 'MMM'}
 
-dic_entero_a_romano= {1:'I' ,2: 'II' ,3: 'III' ,4: 'IV' , 5: 'V' , 6: 'VI' ,  7: 'VII' , 8: 'VIII', 9: 'IX', 10: 'X' , 20: 'XX' , 30: 'XXX' ,  40: 'XV' , 50: 'L' , 60: 'LX' , 70: 'LXX' , 80: 'LXXX' , 90: 'XC', 100: 'C' , 200: 'CC' , 300: 'CCC' ,400: 'CD' , 500: 'D' , 600: 'DC' ,700: 'DCC' , 800: 'DCCC' , 900: 'CM', 1000: 'M' , 2000: 'MM' , 3000: 'MMM'}
+
+dic_entero_a_romano= {1:'I' ,2: 'II' ,3: 'III' ,4: 'IV' , 5: 'V' , 6: 'VI' ,  7: 'VII' , 8: 'VIII', 9: 'IX', 
+                      10: 'X' , 20: 'XX' , 30: 'XXX' ,  40: 'XV' , 50: 'L' , 60: 'LX' , 70: 'LXX' , 80: 'LXXX' , 90: 'XC', 
+                      100: 'C' , 200: 'CC' , 300: 'CCC' ,400: 'CD' , 500: 'D' , 600: 'DC' ,700: 'DCC' , 800: 'DCCC' , 900: 'CM', 
+                      1000: 'M' , 2000: 'MM' , 3000: 'MMM'}
+
 
 class RomanNumberError( Exception ):
     pass
 
-def entero_a_romano(numero):#1994
-    numero = str(numero)
-    list_numero = list(numero)
-   
+#1994
+def entero_a_romano(numero):
+
+    numero = "{:0>4d}".format(numero)#transforma el numero dado a un str de 4 digitos, y si es menos lo complementa con ceros a la izquierda
+    list_numero = list(numero)#transforma el str dado a una lista de string de cada caracter 
     valor_romano = ""
 
     cont = 0
     valor_num = 1000
-    while cont < 4:
+    while cont < len(list_numero):
         list_numero[cont] = int(list_numero[cont])*valor_num
         valor_romano += dic_entero_a_romano.get(list_numero[cont])
         cont+=1
         valor_num /= 10
-    
-    for i in range(0,len(list_numero)):
-        if  i==0:
-            list_numero[i] = int( list_numero[i] )*1000
-            valor_romano += millares.get( list_numero[i])
 
-        elif i==1:
-            list_numero[i] = int( list_numero[i] )*100
-            valor_romano += centenas.get(list_numero[i])
-
-        elif i==2:
-            list_numero[i] = int( list_numero[i] )*10
-            valor_romano += decenas.get(list_numero[i])
-
-        elif i==3: 
-            list_numero[i] = int( list_numero[i] )
-            valor_romano += unidades.get(list_numero[i])
-    ...
-    return valor_romano
-
+   
 print( entero_a_romano(1994) )
